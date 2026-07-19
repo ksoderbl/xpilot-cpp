74bd7956f883b13e24386bcb9b76802e58a0c3a4
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2026-07-19 19:01:10 +0300

    src/server/wildmap.cpp: Remove MAX_MAP_SIZE define.

--- src/server/wildmap.cpp
+++ src/server/wildmap.cpp
@@ -51,10 +51,6 @@
 #define MAPUP(i) map.data[MAPOFFUP(i)]
 #define MAPDOWN(i) map.data[MAPOFFDOWN(i)]
 
-#ifndef MAX_MAP_SIZE
-#define MAX_MAP_SIZE 900
-#endif
-
 #define BLOCK_SPACE ' '
 #define BLOCK_SOLID 'x'
 #define BLOCK_UP_RIGHT 'a'
@@ -149,8 +145,8 @@ static void Option_map(int argc, char **argv)
         double max;
     };
     static struct map_opt mapopts[] = {
-        {"mapWidth", &map.width, 0, 0, 90, MAX_MAP_SIZE},
-        {"mapHeight", &map.height, 0, 0, 90, MAX_MAP_SIZE},
+        {"mapWidth", &map.width, 0, 0, 90, 900},
+        {"mapHeight", &map.height, 0, 0, 90, 900},
         {"seed", 0, &map.seed, 0, 0, MAX_FUZZ},
         {"seedRatio", 0, 0, &map.seed_ratio, 0.02, 0.22},
         {"fillRatio", 0, 0, &map.fill_ratio, 0.02, 0.80},
